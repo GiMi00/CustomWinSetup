8c19118f1b8245599fd6f6d7cc4b3a2865ba6121
GiMi00 <erno.pajala@outlook.com>
2023-09-12 10:59:09 +0300

    Update

--- ProgramSetups/ScoopList.ps1
+++ ProgramSetups/ScoopList.ps1
@@ -6,7 +6,7 @@ scoop bucket add extras
 scoop bucket add nerd-fonts
 
 # Install pkgs
-# scoop install 7zip adb chromium dark fastfetch ffmpeg fzf git grep innounp mpv-git scrcpy ani-cli # More programs
+# scoop install 7zip adb dark fastfetch ffmpeg fzf git grep innounp mpv-git scrcpy ani-cli # More programs
 scoop install 7zip dark ffmpeg git grep innounp mpv-git # Less programs
 scoop install CascadiaCode-NF-Mono
 scoop uninstall git
